876bb911ece166220227bb491de5fae9e9823204
Ghuysu <daogiahuysu@gmail.com>
2024-09-21 20:40:08 +0700

    update server api

--- src/routes/UserProfile.jsx
+++ src/routes/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const UserProfile = () => {
@@ -10,12 +10,15 @@ const UserProfile = () => {
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const response = await fetch(`http://localhost:10000/api/${userId}`, {
-          method: 'GET',
-          headers: {
-            'x-api-key': 'abc-xyz-www',
+        const response = await fetch(
+          `https://skn7vgp9-10000.asse.devtunnels.ms//api/${userId}`,
+          {
+            method: 'GET',
+            headers: {
+              'x-api-key': 'abc-xyz-www',
+            },
           },
-        });
+        );
 
         const data = await response.json();
 
